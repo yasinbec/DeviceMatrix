e28eb1a80b1fb2cbc3b60174f3819dc8f3a6cd5a
Ali Y. Beceren <yasinbec@gmail.com>
2016-11-21 19:32:29 -0500

    fresh start

---
+++ snapCSS.js
@@ -0,0 +1,16 @@
+(function($) {
+
+  // Create plugin
+  $.fn.snapCSS = function() {
+
+    var $html = $('html'),
+      $platform,
+      $device,
+      $version;
+
+
+    return $html.addClass($platform $device $version);
+
+  }
+
+})(jQuery);
